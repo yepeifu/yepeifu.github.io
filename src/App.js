df9906af6918a4f14c9407372d8b319948f4bf61
yepeifu <yepf@htah.com.cn>
2023-03-21 10:40:39 +0800

    修改路径

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
 import './App.css';
 
 function App() {
@@ -6,8 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-       <div>测试一下</div>
+        <Button type="primary">Button</Button>
+        <div>测试一下</div>
       </header>
     </div>
   );
